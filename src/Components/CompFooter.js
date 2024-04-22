de6113c5d8d29d26be33238a97c96b2d31ac7c50
Eli Rosero <elir_94@hotmail.com>
2024-04-22 18:55:19 -0500

    React config

---
+++ src/Components/CompFooter.js
@@ -0,0 +1,18 @@
+// Lo primero que se debe hacer al crear un componente es importar la libreria/framework de React
+import React from "react";
+
+// Dado que los componentes son funciones o métodos, se crean funciones para ellos 
+
+const CompFooter = () => {
+
+    console.log("¡Hola, mundo!")
+    return (
+        <div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default CompFooter; 
